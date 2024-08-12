c172af042ef99b1701a06df3b7096881a98a7ff0
mishainx <fer.r@live.com.ar>
2024-08-12 12:13:50 -0300

    coming soon

--- middleware.js
+++ middleware.js
@@ -3,11 +3,17 @@ import { cookies } from 'next/headers'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
+   const { pathname } = new URL(request.url);
+
+   if (pathname.startsWith('/_next/') || pathname.startsWith('/static/')) {
+      return NextResponse.next();
+    }
+
     const cookieStore = cookies()
     const cookie = cookieStore.get('vg-ct')
 
  if(!cookie){
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL('/proximamente', request.url))
  }
 
 
@@ -15,5 +21,8 @@ return NextResponse.next()
 }
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/admin/:path*',
-}
+   matcher: [
+       // Aplica a todas las rutas excepto `/login`
+       '/((?!login|proximamente).*)',
+   ],
+};
---
+++ src/components/browserIcons/browserIcons.js
@@ -0,0 +1,32 @@
+import InstagramIcon from '@/src/icons/InstagramIcon';
+import FacebookIcon from '@/src/icons/FacebookIcon';
+
+export default function BrowserIcons() {
+  return (
+    <div className="flex justify-center items-center space-x-4">
+      {/* Contenedor de Instagram */}
+      <div className="relative w-12 h-12 flex items-center justify-center cursor-pointer group">
+        {/* Fondo de contorno con box-shadow */}
+        <div className="absolute inset-0 rounded-full border-4 border-yellow-500 shadow-[0_0_12px_2px_rgba(255,255,0,0.75)] transition-all duration-300 ease-in-out transform scale-100 lg:border-gray-500 lg:shadow-none lg:bg-transparent group-hover:border-yellow-500 lg:group-hover:border-yellow-500 lg:group-hover:shadow-[0_0_12px_3px_rgba(255,255,0,0.75)] lg:group-hover:scale-105"></div>
+        <InstagramIcon
+          width={35}
+          height={35}
+          className="text-yellow-500 transition-all duration-300 ease-in-out lg:text-gray-500 lg:group-hover:text-yellow-500"
+        />
+      </div>
+      {/* Contenedor de Facebook */}
+      <div className="relative w-12 h-12 flex items-center justify-center cursor-pointer group">
+        {/* Fondo de contorno con box-shadow */}
+        <div className="absolute inset-0 rounded-full border-4 border-yellow-500 shadow-[0_0_12px_2px_rgba(255,255,0,0.75)] transition-all duration-300 ease-in-out transform scale-100 lg:border-gray-500 lg:shadow-none lg:bg-transparent group-hover:border-yellow-500 lg:group-hover:border-yellow-500 lg:group-hover:shadow-[0_0_12px_3px_rgba(255,255,0,0.75)] lg:group-hover:scale-105"></div>
+        <FacebookIcon
+          width={35}
+          height={35}
+          className="text-yellow-500 transition-all duration-300 ease-in-out lg:text-gray-500 lg:group-hover:text-yellow-500"
+        />
+      </div>
+    </div>
+  );
+}
+
+
+
