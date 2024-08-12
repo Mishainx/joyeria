5f6319b3ea1a213b0a21df2b4f32f6f545c32886
mishainx <fer.r@live.com.ar>
2024-08-12 12:24:05 -0300

    modificaciones

--- app/(admin)/admin/page.js
+++ app/(admin)/admin/page.js
@@ -1,5 +1,4 @@
 import Dashboard from "@/src/components/dashboard/dashBoard";
-import Sidebar from "@/src/components/dashboard/sideBar";
 export default function Admin() {
   return (
     <main className="">
--- src/components/browserIcons/browserIcons.js
+++ src/components/browserIcons/browserIcons.js
@@ -1,10 +1,12 @@
 import InstagramIcon from '@/src/icons/InstagramIcon';
 import FacebookIcon from '@/src/icons/FacebookIcon';
+import Link from 'next/link';
 
 export default function BrowserIcons() {
   return (
     <div className="flex justify-center items-center space-x-4">
       {/* Contenedor de Instagram */}
+      <Link href="https://www.instagram.com/veronicagalainena/" target="_blank">
       <div className="relative w-12 h-12 flex items-center justify-center cursor-pointer group">
         {/* Fondo de contorno con box-shadow */}
         <div className="absolute inset-0 rounded-full border-4 border-yellow-500 shadow-[0_0_12px_2px_rgba(255,255,0,0.75)] transition-all duration-300 ease-in-out transform scale-100 lg:border-gray-500 lg:shadow-none lg:bg-transparent group-hover:border-yellow-500 lg:group-hover:border-yellow-500 lg:group-hover:shadow-[0_0_12px_3px_rgba(255,255,0,0.75)] lg:group-hover:scale-105"></div>
@@ -14,7 +16,9 @@ export default function BrowserIcons() {
           className="text-yellow-500 transition-all duration-300 ease-in-out lg:text-gray-500 lg:group-hover:text-yellow-500"
         />
       </div>
-      {/* Contenedor de Facebook */}
+      </Link>
+
+      <Link href="https://www.facebook.com/veronica.galainena/?locale=es_LA" target="_blank">
       <div className="relative w-12 h-12 flex items-center justify-center cursor-pointer group">
         {/* Fondo de contorno con box-shadow */}
         <div className="absolute inset-0 rounded-full border-4 border-yellow-500 shadow-[0_0_12px_2px_rgba(255,255,0,0.75)] transition-all duration-300 ease-in-out transform scale-100 lg:border-gray-500 lg:shadow-none lg:bg-transparent group-hover:border-yellow-500 lg:group-hover:border-yellow-500 lg:group-hover:shadow-[0_0_12px_3px_rgba(255,255,0,0.75)] lg:group-hover:scale-105"></div>
@@ -24,6 +28,9 @@ export default function BrowserIcons() {
           className="text-yellow-500 transition-all duration-300 ease-in-out lg:text-gray-500 lg:group-hover:text-yellow-500"
         />
       </div>
+      </Link>
+
+
     </div>
   );
 }
