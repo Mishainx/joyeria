51b945eaecb050d6de05fc8daa9f989109454dec
mishainx <fer.r@live.com.ar>
2024-08-14 21:26:30 -0300

    modificaciones

--- app/(shop)/page.js
+++ app/(shop)/page.js
@@ -1,7 +1,5 @@
-import Footer from "@/src/components/footer/footer";
 import Gallery from "@/src/components/gallery/gallery";
 import Hero from "@/src/components/hero/hero";
-import Loader from "@/src/components/Loader/Loader";
 
 export default function Home() {
   return (
