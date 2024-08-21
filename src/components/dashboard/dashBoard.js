3b17957432c83ca5ca1a393b8cefafee474a0b00
mishainx <fer.r@live.com.ar>
2024-08-21 09:13:36 -0300

    modificaciones

--- src/components/dashboard/dashBoard.js
+++ src/components/dashboard/dashBoard.js
@@ -2,13 +2,21 @@
 // Dashboard.jsx
 import { useState } from 'react';
 import Prueba from './Prueba';
+import Sidebar from './Sidebar';
+import Content from './Content';
 
 const Dashboard =  ({categories}) => {
   const [selectedContent, setSelectedContent] = useState('products');
 
+    // Función onSelect que actualiza el estado
+    const onSelect = (content) => {
+      setSelectedContent(content);
+    };
+
   return (
     <div className="flex min-h-screen">
-      <Prueba/>
+      <Sidebar onSelect={onSelect}/>
+      <Content selectedContent={selectedContent}/>
     </div>
   );
 };
