8173762784f32cd93791539988a07b02da6abe8c
mishainx <fer.r@live.com.ar>
2024-08-21 09:01:07 -0300

    modificaciones

--- src/components/dashboard/dashBoard.js
+++ src/components/dashboard/dashBoard.js
@@ -1,16 +1,12 @@
 "use client"
 // Dashboard.jsx
 import { useState } from 'react';
-import Sidebar from './Sidebar';
-import Content from '@/src/components/dashboard/Sidebar';
 
 const Dashboard =  ({categories}) => {
   const [selectedContent, setSelectedContent] = useState('products');
 
   return (
     <div className="flex min-h-screen">
-      <Sidebar onSelect={setSelectedContent} />
-      <Content selectedContent={selectedContent} categories={categories} />
     </div>
   );
 };
