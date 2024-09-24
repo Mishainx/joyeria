f2606f9bc3ac9c477c14d53146b406ba08dfdc2b
mishainx <fer.r@live.com.ar>
2024-09-24 00:10:01 -0300

    modificaciones

--- src/components/products/CatalogueContainer.js
+++ src/components/products/CatalogueContainer.js
@@ -1,12 +1,10 @@
 // src/components/FeaturedProducts.jsx
-import { fetchProducts } from '@/src/utils/productManager';
 import CatalogueListProducts from './CatalogueListProducts';
 
 export default async function CatalogueContainer() {
-    const products = await fetchProducts();
-    {/*const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {cache:"no-store"}); // Ajusta la URL según sea necesario
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {cache:"no-store"}); // Ajusta la URL según sea necesario
     const data = await response.json()
-    const products = data.payload;*/}
+    const products = data.payload
 
   return (
     <CatalogueListProducts catalogueProducts={products}/>
