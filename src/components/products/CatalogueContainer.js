3cd7928a5072cb2e9a0854477710e743b4d716b1
mishainx <fer.r@live.com.ar>
2024-09-23 22:32:46 -0300

    modificaciones

--- src/components/products/CatalogueContainer.js
+++ src/components/products/CatalogueContainer.js
@@ -2,16 +2,8 @@
 import { fetchProducts } from '@/src/utils/productManager';
 import CatalogueListProducts from './CatalogueListProducts';
 
-export default async function CatalogueContainer({categories}) {
-  let products = [];
-
-  try {
-    products = await fetchProducts();
-  } catch (error) {
-    console.error('Error al cargar productos:', error);
-  }
-
-  const featuredProducts = products.filter((product) => product.featured) || [];
+export default async function CatalogueContainer() {
+const products = await fetchProducts()
 
   return (
     <CatalogueListProducts catalogueProducts={products}/>
