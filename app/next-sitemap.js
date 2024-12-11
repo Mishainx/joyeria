22ba36790c44503bfc73efc89b4b58d5c4bebf6c
mishainx <fer.r@live.com.ar>
2024-12-11 08:14:48 -0300

    modificaciones

---
+++ app/next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://www.veronicagalainena.com.ar', // Cambia esta URL a la de tu sitio web
+    generateRobotsTxt: true, // Si quieres generar el archivo robots.txt también
+    changefreq: 'weekly', // Frecuencia de cambio de las páginas
+    priority: 0.7, // Prioridad de las páginas
+    sitemapSize: 5000, // Número máximo de URLs por archivo sitemap
+  }
+  
